5d90e905483ea0b56ffe6c75b28931c52eb26cae
YDZ <ydz627@gmail.com>
2018-01-29 17:01:38 +0800

    add device

--- android/ocr/devices.py
+++ android/ocr/devices.py
@@ -14,4 +14,5 @@ CONFIGS = {
     'LGE Nexus 5': Namespace(sw=1080, sh=1920, x0=141, y0=577,  w=144, h=112,  dx=219, dy=292,  tx=310, ty=128, tw=460, th=188),
     'OnePlus A0001': Namespace(sw=1080, sh=1920, x0=182, y0=608,  w=112, h=96,  dx=202.5, dy=270,  tx=330, ty=172, tw=440, th=174),
     'Motorola Moto G4 Plus': Namespace(sw=1080, sh=1920, x0=166, y0=596,  w=143, h=125,  dx=203, dy=270,  tx=367, ty=173, tw=346, th=160),
+    'Xiaomi MI 3': Namespace(sw=1080, sh=1920, x0=166, y0=596,  w=143, h=125,  dx=203, dy=270,  tx=367, ty=173, tw=346, th=160),
 }
