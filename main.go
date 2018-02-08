3e9e7f4cca1dbe86e2e842c7ce876d3ce83d062c
YDZ <ydz627@gmail.com>
2018-02-09 07:33:38 +0800

    receive websocket data

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/gorilla/websocket"
 	"github.com/halfrost/threes-ai/ai"
+	"github.com/halfrost/threes-ai/gameboard"
 	"github.com/halfrost/threes-ai/utils"
 )
 
@@ -35,10 +36,15 @@ var (
 	online int32
 )
 
+// RevData define
+type RevData struct {
+	Data [][]int
+	Next int
+}
+
 func main() {
 	fmt.Println(logo)
 	utils.InitGameScoreTable()
-	ai.InitGameState()
 	flag.Parse()
 	log.SetFlags(0)
 	http.HandleFunc("/compute", compute)
@@ -69,16 +75,20 @@ func compute(w http.ResponseWriter, r *http.Request) {
 			break
 		}
 		log.Printf("recv message: %s\n", string(p))
-		// g := &grid.Grid{}
-		// if err = json.Unmarshal(p, g); err != nil {
-		// 	break
-		// }
-		// a := &ai.AI{Grid: g}
-		// dire := a.Search()
-		// result := map[string]grid.Direction{"dire": dire}
-		// p, _ = json.Marshal(result)
-		result := make(map[string]string, 0)
-		result["data"] = "这是服务器返回信息"
+		d := &RevData{}
+		if err = json.Unmarshal(p, d); err != nil {
+			break
+		}
+		fmt.Printf("接收到的数据是 = %v\n", d)
+		nextBrick := make([]int, 0)
+		nextBrick = append(nextBrick, d.Next)
+		cand := gameboard.FindCandidates(d.Data)
+
+		utils.PrintfGame(d.Data, cand, nextBrick)
+		move := ai.ExpectSearch(d.Data, cand, nextBrick)
+
+		result := make(map[string]int, 0)
+		result["dire"] = move
 		p, _ = json.Marshal(result)
 		if err := conn.WriteMessage(messageType, p); err != nil {
 			break
@@ -87,16 +97,17 @@ func compute(w http.ResponseWriter, r *http.Request) {
 }
 
 //export search_move
-func search_move(board uint64, deck uint32, tileset uint16) float64 {
-	cand, _ := utils.GetCandidates(deck)
-	ai.ExpectSearch(utils.GetBoard(board), cand, utils.GetNextBrick(tileset))
-	return 0
+func search_move(board uint64, deck uint32, tileset uint16) int {
+	//cand, _ := utils.GetCandidates(deck)
+	b := utils.GetBoard(board)
+	move := ai.ExpectSearch(b, gameboard.FindCandidates(b), utils.GetNextBrick(tileset))
+	fmt.Printf("执行的移动是 = %v\n\n\n\n\n", move)
+	return move
 }
 
 //export init_game
 func init_game() {
 	utils.InitGameScoreTable()
-	ai.InitGameState()
 	fmt.Printf("【初始化】\n")
 }
 
